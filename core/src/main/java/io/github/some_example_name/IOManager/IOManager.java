12fa3fc560f3bce129db086af2f20fa77ba97b11
dayryyl <daryltanzj@gmail.com>
2026-02-09 19:28:57 +0800

    IOManager

---
+++ core/src/main/java/io/github/some_example_name/IOManager/IOManager.java
@@ -0,0 +1,31 @@
+package io.github.some_example_name.iomanager;
+
+public class IOManager {
+
+    private Mouse mouse; //Mouse is class, mouse is object
+    private Keyboard keyboard;
+    private Audio audio;
+
+    public IOManager() {
+        this.mouse = new Mouse();
+        this.keyboard = new Keyboard();
+        this.audio = new Audio();
+    }
+    
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public Audio getAudio() {
+        return audio;
+    }
+    
+    public void log(String msg) {
+        System.out.println("[ENGINE LOG]: " + msg);
+    }
+}
+
