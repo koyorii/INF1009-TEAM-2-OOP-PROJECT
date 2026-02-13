37fd48ef6b3f2bc9386abd8604bff1dadb12841b
dayryyl <daryltanzj@gmail.com>
2026-02-13 14:42:39 +0800

    keyboard

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Keyboard.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Keyboard.java
@@ -1,5 +1,18 @@
 package io.github.some_example_name.lwjgl3.iomanager;
 
+import com.badlogic.gdx.Gdx;
+
 public class Keyboard {
+
+    public Keyboard() { //Constructor
+    }
+
+    public boolean isKeyPressed(int keycode) {
+        return Gdx.input.isKeyPressed(keycode); //keycode for the key ID you want to check.
+    }
+
+    public boolean isKeyJustPressed(int keycode) {
+        return Gdx.input.isKeyJustPressed(keycode);
+    }
 
 }
