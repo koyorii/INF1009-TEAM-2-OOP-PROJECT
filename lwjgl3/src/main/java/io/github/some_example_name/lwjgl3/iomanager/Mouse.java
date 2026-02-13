b756b4b77c9edd4bfbe8cbc9260a388653db7d14
dayryyl <daryltanzj@gmail.com>
2026-02-13 15:26:42 +0800

    mouse

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Mouse.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Mouse.java
@@ -1,8 +1,32 @@
 package io.github.some_example_name.lwjgl3.iomanager;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+
 public class Mouse {
 
-    public void update(); {
-        
+    private Vector2 position;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
+
+    public Mouse() {
+        this.position = new Vector2(0, 0);
+        this.isLeftPressed = false;
+        this.isRightPressed = false;
+    }
+
+    public void update() {
+        this.position.set(Gdx.input.getX(), Gdx.input.getY()); //updates position
+        this.isLeftPressed = Gdx.input.isButtonPressed(Input.Buttons.LEFT);
+        this.isRightPressed = Gdx.input.isButtonPressed(Input.Buttons.RIGHT);
+    }
+
+    public Vector2 getMousePosition() {
+        return this.position;
+    }
+
+    public boolean isButtonDown(int code) {
+        return Gdx.input.isButtonPressed(code);
     }
 }
