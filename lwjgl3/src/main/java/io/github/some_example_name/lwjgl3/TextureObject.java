2f36ea4742d391a608f45ffa93659873fd5c7651
koyorii <53043482+koyorii@users.noreply.github.com>
2026-02-14 13:52:13 +0800

    boarder and collision resolve

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/TextureObject.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/TextureObject.java
@@ -18,7 +18,7 @@ public class TextureObject extends MovableEntity {
     }
 
     // Parameterized Constructor
-    public TextureObject(String path, float x, float y, float speed,boolean isFalling) {
+    public TextureObject(String path, float x, float y, float speed, boolean isFalling) {
         super(x, y, speed, null); // Call parent constructor
         this.tex = new Texture(Gdx.files.internal(path));
         this.isFalling = isFalling;
@@ -38,47 +38,63 @@ public class TextureObject extends MovableEntity {
     // Getter and Setter for texture
     public Texture getTexture() { return tex; }
     public void setTexture(Texture t) { this.tex = t; }
-    public boolean getIsFalling() {return isFalling;}
-    
+    public boolean getIsFalling() { return isFalling; }
+
     // Movement method for bucket (Overriding - controlled by LEFT and RIGHT arrow keys)
     @Override
     public void movement() {
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float w = tex.getWidth();
+        float h = tex.getHeight();
+
         if (isFalling) {
             y -= speed;
-            if (y < 0) y = 480;
+            // Wrap back to top if it falls below the screen
+            if (y + h < 0) y = screenHeight;
+
+            // Clamp horizontal position within screen bounds
+            if (x < 0) x = 0;
+            if (x + w > screenWidth) x = screenWidth - w;
         } else {
             // Bucket movement
             if (Gdx.input.isKeyPressed(Keys.LEFT)) x -= speed;
             if (Gdx.input.isKeyPressed(Keys.RIGHT)) x += speed;
+
+            // Clamp bucket within screen bounds
+            if (x < 0) x = 0;
+            if (x + w > screenWidth) x = screenWidth - w;
+            if (y < 0) y = 0;
+            if (y + h > screenHeight) y = screenHeight - h;
         }
     }
-    
+
     // Movement method for droplets (Overloading - different signature)
-    // This demonstrates method overloading - same class, different method signatures
     public void movement(boolean isFalling) {
         if (isFalling) {
             y -= speed;
-            
+
             // Reset if it hits the bottom
             if (y < 0) {
-                y = 480;
+                y = Gdx.graphics.getHeight();
             }
         }
     }
+
     @Override
-    public void update(){
+    public void update() {
         String type = isFalling ? "Droplet" : "Bucket";
         System.out.println("In TextureObject of " + type + " at " + super.getX() + "," + super.getY() + " position");
     }
 
     @Override
-    public Polygon getBounds(){
+    public Polygon getBounds() {
         rectPolygon.setPosition(x, y);
         return rectPolygon;
     }
 
     @Override
-    public void draw(SpriteBatch batch){
+    public void draw(SpriteBatch batch) {
         batch.draw(this.tex, super.getX(), super.getY(), this.tex.getWidth(), this.tex.getHeight());
     }
 }
