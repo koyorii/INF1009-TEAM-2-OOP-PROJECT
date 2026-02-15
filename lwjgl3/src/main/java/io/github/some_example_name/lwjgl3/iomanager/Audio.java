b4cf01b5b253ab32120f7f2204fc36387974939d
dayryyl <daryltanzj@gmail.com>
2026-02-15 19:55:03 +0800

    audio

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Audio.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/iomanager/Audio.java
@@ -2,4 +2,27 @@ package io.github.some_example_name.lwjgl3.iomanager;
 
 public class Audio {
 
+    private String path;
+    private boolean isPlaying;
+
+    public Audio() { //constructor
+        this.path = "";
+        this.isPlaying = false;
+    }
+
+    public void playAudio(String path) {
+        this.path = path;
+        System.out.println("Playing audio from: " + path);
+        this.isPlaying = true;
+
+    }
+
+    public void stopAudio() {
+        System.out.println("Stopping audio from: " + path);
+        this.isPlaying = false;
+    }
+
+    public boolean isPlaying() {return isPlaying;}
+
+    public String getPath() {return path;}
 }
