b9918a05b9ec32b585f0545ba709533bbb465873
TypeAlyce <typealyce@gmail.com>
2026-03-19 00:53:25 +0800

    Added difficulty Scene

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/SceneMenu.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/SceneMenu.java
@@ -21,7 +21,7 @@ public class SceneMenu extends Scene{
     public SceneMenu(ISceneManager ism) {
         super(ism);
 
-        // Initialise the Stage, direct keys to UI stage, set up containers like in HTML, apply styling, and listener for button press to go to GAME scene
+        // Initialise the Stage, direct keys to UI stage, set up containers like in HTML, apply styling, and listener for button press to go to GAMEscene
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
