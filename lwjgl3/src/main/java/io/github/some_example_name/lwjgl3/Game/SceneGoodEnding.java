e2167019d2b537fba491436b6dec13634f910639
TypeAlyce <typealyce@gmail.com>
2026-03-21 20:02:56 +0800

    deleted playerNPC

---
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Game/SceneGoodEnding.java
@@ -0,0 +1,15 @@
+//package io.github.some_example_name.lwjgl3.Game;
+//
+//import com.badlogic.gdx.scenes.scene2d.Stage;
+//import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+//import io.github.some_example_name.lwjgl3.Engine.sceneManager.ISceneManager;
+//import io.github.some_example_name.lwjgl3.Engine.sceneManager.Scene;
+//
+//public class SceneGoodEnding extends Scene {
+//    private final Stage stage;
+//    private final Skin skin;
+//
+//    public SceneGoodEnding(ISceneManager ism, PlayerStats ps) {
+//        super(ism);
+//    }
+//}
